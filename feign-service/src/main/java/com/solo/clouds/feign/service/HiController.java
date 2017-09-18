bbd4f1dfba60292b5c73b37f2e0415d4f9771a63
henrysolo <zhang89jt@gmail.com>
2017-09-18 17:42:24 +0800

    init commit

---
+++ feign-service/src/main/java/com/solo/clouds/feign/service/HiController.java
@@ -0,0 +1,22 @@
+package com.solo.clouds.feign.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhangjt13666 on 2017/9/18.
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ feign-service/src/main/java/com/solo/clouds/feign/service/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package com.solo.clouds.feign.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by zhangjt13666 on 2017/9/18.
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+}
